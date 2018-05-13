6f872adf2b1fee9695588a5d327f2e8dcc5e4b3c
JuliaKondratenko <36399932+JuliaKondratenko@users.noreply.github.com>
2018-05-13 18:59:15 +0300

    form

--- mail-it.php
+++ mail-it.php
@@ -1,17 +1,18 @@
-<?
-$name = $_POST{'name'};
-$email = $_POST{'email'};
-$message = $_POST{'message'};
-
-
-$email_message = "
-Новая заявка!
-
-Имя: ".$name."
-Email: ".$email."
-Сообщения: ".$message."
-
-
-";
-
-mail ("juliet.kondratenko@gmail.com" , "Новая заявка", $email_message);?>
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$name = htmlspecialchars($name);
+$email = htmlspecialchars($email);
+$name = urldecode($name);
+$email = urldecode($email);
+$name = trim($name);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("juliet.kondratenko@gmail.com", "Заявка с сайта", "Имя:".$name.". E-mail: ".$email ,"From: \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
